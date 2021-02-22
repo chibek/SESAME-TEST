e81ce8e33730255932084984f3d1b8cea2f7a096
sergio <sergibelemarketing@gmail.com>
2021-02-22 23:11:25 +0100

    Add tailwind colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
+const { colors: tailwindColors } = require("tailwindcss/defaultTheme");
+
+const colors = {
+  ...tailwindColors,
+  primary: { DEFAULT: "#5EBEA3", hover: "#BEF0E2" },
+  secondary: "#FF9984",
+  dark: {
+    primary: {
+      DEFAULT: "#788387",
+      hover: "#D3E6ED",
+    },
+  },
+  bg: "#B5B5B5",
+  screen: "#F4F4F4",
+};
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'class',
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      fill: colors,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
